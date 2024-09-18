9d5ad45492845b21f831fc34814d5951ffafd042
Joseph Simutis <js8555240@gmail.com>
2024-09-18 10:19:53 -0400

    Added a bunch more things

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,9 +1,13 @@
 package io.github.josephsimutis
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.terminal
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.int
+import com.github.ajalt.mordant.terminal.ConversionResult
+import com.github.ajalt.mordant.terminal.prompt
 import java.net.Socket
+import java.util.Scanner
 
 class Client : CliktCommand() {
     val address by argument(help="The address of the server")
@@ -11,6 +15,20 @@ class Client : CliktCommand() {
 
     override fun run() {
         val client = Socket(address, port)
-        client.getInputStream()
+        val receiveThread = Thread {
+            val stream = client.getInputStream().bufferedReader()
+            while(!client.isClosed) {
+                terminal.println(stream.readLine())
+            }
+        }
+        receiveThread.start()
+        val stream = client.getOutputStream().bufferedWriter()
+        val scanner = Scanner(System.`in`)
+        while(!client.isClosed) {
+            terminal.prompt("> ", promptSuffix = "") { message ->
+                stream.write(message)
+                ConversionResult.Valid(message)
+            }
+        }
     }
 }
--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,8 +1,48 @@
 package io.github.josephsimutis
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.terminal
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.types.int
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
 
 class Server : CliktCommand() {
+    val port by argument(help="The port of the server").int()
+    lateinit var server: ServerSocket
+    val clients = ArrayList<ClientHandler>()
+
     override fun run() {
+        server = ServerSocket(port)
+        terminal.println("Server started on port $port!")
+        while (!server.isClosed) {
+            val client = ClientHandler(server.accept(), this)
+            clients += client
+            Thread(client).start()
+            terminal.println("Client connected!")
+        }
+        clients.forEach {
+            it.socket.close()
+        }
+    }
+
+    class ClientHandler(val socket: Socket, val server: Server) : Runnable {
+        override fun run() {
+            val stream = socket.getInputStream().bufferedReader()
+            var open = true
+            while (open) {
+                val message = stream.readLine()
+                if (message == null || message.trim() == "") open = false
+                else {
+                    for (receiver in server.clients) {
+                        PrintWriter(receiver.socket.getOutputStream()).write(message)
+                    }
+                    server.terminal.println(message)
+                }
+            }
+            server.terminal.println("Client disconnected!")
+        }
     }
 }
